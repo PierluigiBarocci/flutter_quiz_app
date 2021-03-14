0c1acd32ea3e44143e307ab8615b91fe9c8c7c7d
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-03-14 12:38:39 +0100

    feat: create Home widget and refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
@@ -10,8 +8,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Home(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        backgroundColor: Colors.indigo.shade800,
+      ),
+      body: Text('This is my defaul text'),
     );
   }
 }
